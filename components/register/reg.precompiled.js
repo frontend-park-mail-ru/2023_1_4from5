b79e8372c7bb50158a2adda745c7f2da9e806cf6
Alik Nigmatullin <NigAlik020503@yandex.ru>
2023-03-02 00:57:21 +0300

    eddited reg, started validation

--- components/register/reg.precompiled.js
+++ components/register/reg.precompiled.js
@@ -1,6 +1,6 @@
 (function() {
   var template = Handlebars.template, templates = Handlebars.templates = Handlebars.templates || {};
 templates['reg'] = template({"compiler":[8,">= 4.3.0"],"main":function(container,depth0,helpers,partials,data) {
-    return "<div id=\"register\">\n    <div>\n        <label for=\"username\">Логин</label>\n        <input type=\"text\">\n    </div>\n    <div>\n        <label for=\"password\">Пароль</label>\n        <input type=\"password\">\n    </div>\n    <div>\n        <label for=\"password\">Повторите пароль</label>\n        <input type=\"password\">\n    </div>\n    <div>\n        <input type=\"submit\">\n    </div>\n</div>";
+    return "<div id=\"register\">\n    <div>\n        <label for=\"username\">Логин</label>\n        <input id=\"reg-username\" type=\"text\">\n    </div>\n    <div>\n        <label for=\"password\">Пароль</label>\n        <input id=\"reg-password\" type=\"password\">\n    </div>\n    <div>\n        <label for=\"password\">Повторите пароль</label>\n        <input id=\"reg-passwordCheck\" type=\"password\">\n    </div>\n    <div>\n        <input id=\"reg-btn\" type=\"submit\">\n    </div>\n</div>";
 },"useData":true});
 })();
--- index.js
+++ index.js
@@ -91,8 +91,56 @@ function renderRegister(parent) {
     const reg = new Register(parent);
     reg.render();
     console.log('Register rendered');
+
+    const submitBtn = document.getElementById('reg-btn');
+    const usernameInput = document.getElementById('reg-username');
+    const passwordInput = document.getElementById('reg-password');
+    const passwordInputCheck = document.getElementById('reg-passwordCheck');
+
+    submitBtn.addEventListener( "click", (e) => {
+        e.preventDefault();
+        const username = usernameInput.value;
+        console.log(username);
+        const password = passwordInput.value;
+        console.log(password);
+        const passwordCheck = passwordInputCheck.value;
+        console.log(passwordCheck);
+
+
+
+        if (password === passwordCheck) {
+            fetch ('sub-me.ru:8000/api/auth/signUp', {
+                method: 'POST',
+                mode: 'cors',
+                credentials: 'include',
+                data: JSON.stringify({
+                    "login": username,
+                    "password_hash": password,
+                })
+            })
+                .then(response => console.log(response.ok))
+        }
+    });
 }
 
 renderSideBar(sideBarElement);
 renderAuth(rootElement);
 renderRegister(rootElement);
+
+function isValid(inputStr) {
+    const blackList = ""; //надо ли???
+    let hasUpper=false, hasLower = false, hasNumber = false, hasSpecial = false,
+        hasntBlackList = true, hasMinLen = false;
+    if (inputStr.length >= 7) {
+        hasMinLen = true;
+    }
+    for (const char in inputStr) {
+        if (!~blackList.indexOf(char)) {
+            hasntBlackList = false;
+        }
+
+    }
+
+    return hasMinLen && hasNumber && hasUpper && hasLower && hasSpecial && hasntBlackList;
+
+}
