ada90cd2190f65b9239856d895ca1ba73b07e999
IvanStukalov <ivanstukalov2003@gmail.com>
2023-03-02 19:50:07 +0300

    login finished

--- components/sideBar/sideBar.js
+++ components/sideBar/sideBar.js
@@ -15,30 +15,15 @@ export class SideBar {
     }
 
     render() {
-        var template = Handlebars.templates.sideBar;
-        var tScript = template(this.#config);
-
-        this.#parent.innerHTML += tScript;
-
-        // const authorBtn = document.getElementById('author');
-
-        // authorBtn.addEventListener( 'click', (e) => {
-        //     e.preventDefault();
-        //     console.log('eventListener');
-        //     fetch ('http://sub-me.ru:8000/api/user/profile', {
-        //         method: 'GET',
-        //         headers: {
-        //             "Content-Type": "application/json",
-        //         },
-        //         body: JSON.stringify({
-        //             "user_id": "b184cc4e-78ef-434f-ac88-5084a77ee085",
-        //         }),
-        //     })
-        //     .then(response => console.log(response))
-        // });
+        let lastSideBar = document.getElementById('sidebarDiv');
+        if (lastSideBar) {
+            lastSideBar.remove();
+        }
+        let newDiv = document.createElement('div');
+        newDiv.id = 'sidebarDiv';
+        let template = Handlebars.templates.sideBar;
+        let tScript = template(this.#config);
+        newDiv.innerHTML = tScript;
+        this.#parent.appendChild(newDiv);
     }  
 }
-
-Handlebars.registerHelper('stringifyFunc', function(renderElement) {
-    return new Handlebars.SafeString("(" + renderElement.toString() + ")()");
-});
