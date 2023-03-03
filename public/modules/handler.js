d460aea6ff21fe7a66eedd2d3d0cb6f4295f1100
IvanStukalov <ivanstukalov2003@gmail.com>
2023-03-03 22:36:36 +0300

    Lelya`s edits

---
+++ public/modules/handler.js
@@ -0,0 +1,29 @@
+export function clickHandler (e, configPart, config) {
+    if (e.target instanceof HTMLAnchorElement) {
+        e.preventDefault();
+        const targetId = e.target.id;
+        let target;
+        // console.log(e.target.id);
+        // console.log(key, config[key], config[key].pages);
+        // config.key.pages
+        // console.log(config.general)
+        configPart.pages.forEach(element => {
+            // console.log(element.id, targetId, typeof element.id, typeof targetId )
+            if (element.id === targetId) {
+                // console.log(element, config[key]);
+                target = element;
+            }
+        });
+
+        if (config.activePage === target.name) {
+            return;
+        }
+
+        if (!(target.name === 'Регистрация' || target.name === 'Войти' || target.name === config.user.username)) {
+            target.parent.innerHTML = '';
+        }
+        config.activePage = target.name;
+        // console.log(config.activePage);
+        target.render(target.parent);
+    }
+}
