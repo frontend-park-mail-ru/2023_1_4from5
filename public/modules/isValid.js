863c5607e81d903c8c74d89c0f08942ca589d1ac
IvanStukalov <ivanstukalov2003@gmail.com>
2023-03-07 20:35:27 +0300

    explain ascii codes

--- public/modules/isValid.js
+++ public/modules/isValid.js
@@ -1,3 +1,45 @@
+const REQUIREMENTS = {
+    LENGTH : {
+        MIN : 7,
+        MAX : 20,
+    },
+    ASCII : {
+        UPPER_A : 65,
+        UPPER_Z : 90,
+        LOWER_A : 97,
+        LOWER_Z : 122,  
+        EXCLAMATION : 33,
+        SLASH : 47,
+        COLON : 58,
+        AT : 64,
+        RECTANGLE_BRACKET : 91,
+        BACK_QUOTE : 96,
+        FIGURED_BRACKET : 123,
+        TILDE : 126,   
+    }
+}
+
+/**
+ * check for letter
+ * @param {int} inputStr - ASCII code of sign
+ *
+ * @returns {bool} - response is sign is letter
+ */
+function isLetter(code) {
+    return (code >= REQUIREMENTS.ASCII.LOWER_A && code <= REQUIREMENTS.ASCII.LOWER_Z || code >= REQUIREMENTS.ASCII.UPPER_A && code <= REQUIREMENTS.ASCII.UPPER_Z);
+}
+
+/**
+ * check for special sign
+ * @param {int} inputStr - ASCII code of sign
+ *
+ * @returns {bool} - response is sign is special sign
+ */
+function isSpecialSign(code) {
+    return (((code >= 32 && code <= 48) || (code >= 57 && code <= 65))
+            || ((code >= 90 && code <= 97) || (code >= 122 && code <= 127)));
+}
+
 /**
  * validation of password input
  * @param {String} inputStr - injected password
@@ -27,7 +69,7 @@ export function isValidPassword(inputStr) {
             error: 'Пароль должен содержать хотя бы 1 спец. символ',
         },
     };
-    if (inputStr.length >= 7) {
+    if (inputStr.length >= REQUIREMENTS.LENGTH.MIN) {
         flags.hasMinLen.flag = true;
     } else {
         return flags.hasMinLen.error;
@@ -37,10 +79,9 @@ export function isValidPassword(inputStr) {
         const code = char.charCodeAt(0);
         if (!isNaN(char)) {
             flags.hasNumber.flag = true;
-        } else if (code > 96 && code < 123 || code > 64 && code < 91) {
+        } else if (isLetter(code)) {
                 flags.hasLower.flag = true;
-        } else if (((code > 32 && code < 48) || (code > 57 && code < 65))
-            || ((code > 90 && code < 97) || (code > 122 && code < 127))) {
+        } else if (isSpecialSign(code)) {
             flags.hasSpecial.flag = true;
         }
     }
@@ -69,10 +110,10 @@ export function isValidLogin(inputStr) {
             error: 'Логин не должен содержать более 20 символов',
         },
     };
-    if (inputStr.length < 7) {
+    if (inputStr.length < REQUIREMENTS.LENGTH.MIN) {
         return flags.hasMinLen.error;
     }
-    if (inputStr.length > 20) {
+    if (inputStr.length > REQUIREMENTS.LENGTH.MAX) {
         return flags.hasMaxLen.error;
     }
 }
