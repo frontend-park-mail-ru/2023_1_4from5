3904c00a27ff28b4bc88f8513d7def030eaa915d
IvanStukalov <ivanstukalov2003@gmail.com>
2023-03-31 17:40:07 +0300

    finished flux

--- public/index.js
+++ public/index.js
@@ -1,17 +1,15 @@
-import { Register } from './components/register/reg.js';
 import { sideBar } from './components/sideBar/sideBar.js';
-import { constructConfig } from './modules/constructConfig.js';
-import { setConfig } from './consts/constants.js';
+import { startPage } from './components/startPage/startPage.js';
+import { Register } from './components/register/reg.js';
 import { request } from './modules/request.js';
 import { authStore } from './store/authStore.js'; // не удалять!!! он создает authStore!!! (потом починим)
-import { sideBarStore } from "./store/sideBarStore.js"; // не удалять!!! он создает sidebarStore!!! (потом починим)
 import { userStore } from './store/userStore.js';
 import { Actions } from './actions/auth.js';
 import { startStore } from './store/startStore.js';
+import { sideBarStore } from "./store/sideBarStore.js"; // не удалять!!! он создает sidebarStore!!! (потом починим)
 import { winSettingsStore } from './store/winsettingsStore.js';
 import { winSettings } from './components/winSettings/winSettings.js';
 import { regStore } from './store/regStore.js';
-import { startPage } from './components/startPage/startPage.js';
 import { settingsStore } from './store/settingsStore.js';
 import { myPageStore } from './store/myPageStore.js';
 
