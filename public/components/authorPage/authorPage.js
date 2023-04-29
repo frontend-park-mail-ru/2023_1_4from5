770f6b5b681b0d5bf0ec422bbe650900489471cc
IvanStukalov <ivanstukalov2003@gmail.com>
2023-04-29 17:05:11 +0300

    author page

--- public/components/authorPage/authorPage.js
+++ public/components/authorPage/authorPage.js
@@ -33,18 +33,10 @@ class AuthorPage {
     newDiv.innerHTML = template(this.#config);
     this.#parent.appendChild(newDiv);
 
-    const backGnd = document.getElementById('myPage-header-container');
-    backGnd.style.backgroundImage = 'url(../../images/myPage_phone.jpg)';
+    const backGnd = document.getElementById('author__header');
+    backGnd.style.backgroundImage = 'url(../../images/cover-photo.svg)';
 
-    const settingsIcon = document.getElementById('settings-icon');
-    if (settingsIcon !== null) {
-      settingsIcon.addEventListener('click', (e) => {
-        e.preventDefault();
-        router.go(URLS.authorSettings);
-      });
-    }
-
-    const createPostBtn = document.getElementById('createPost-btn');
+    const createPostBtn = document.getElementById('create__post');
     if (createPostBtn) {
       createPostBtn.addEventListener('click', (e) => {
         e.preventDefault();
@@ -52,63 +44,64 @@ class AuthorPage {
       });
     }
 
-    const deletePostBtns = document.querySelectorAll('#delete-icon');
+    const deletePostBtns = document.querySelectorAll('#delete__post');
     for (let index = 0; index < deletePostBtns.length; index++) {
       const button = deletePostBtns[index];
       button.addEventListener('click', this.deleteHandler);
     }
 
-    const updatePostBtns = document.querySelectorAll('#pencil-icon');
+    const updatePostBtns = document.querySelectorAll('#edit__post');
     for (let index = 0; index < updatePostBtns.length; index++) {
       const button = updatePostBtns[index];
       button.addEventListener('click', this.updateHandler.bind(this));
     }
 
-    const likeIcons = document.querySelectorAll('.like-icon');
+    const likeIcons = document.querySelectorAll('.icon--like');
     for (let index = 0; index < likeIcons.length; index++) {
       const likeIcon = likeIcons[index];
       likeIcon.addEventListener('click', (event) => {
         const eventLike = likeIcon.id === 'love-like-icon' ? 'removeLike' : 'addLike';
-        Actions.clickLike(eventLike, event.target.parentElement.parentElement.parentElement.id);
+        Actions.clickLike(eventLike,
+          event.target.parentElement.parentElement.parentElement.parentElement.id);
       });
     }
+    //
+    // const editAimIcon = document.getElementById('pencil-icon-aim');
+    // if (editAimIcon) {
+    //   editAimIcon.addEventListener('click', (e) => {
+    //     e.preventDefault();
+    //     Actions.editAim();
+    //     const aimInput = document.getElementById('description-edit-aim');
+    //     aimInput.textContent = this.#config.aim.description;
+    //   });
+    // }
+    //
+    // const closeEditAimIcon = document.getElementById('close-icon-aim');
+    // if (closeEditAimIcon) {
+    //   closeEditAimIcon.addEventListener('click', (e) => {
+    //     e.preventDefault();
+    //     Actions.closeEditAim();
+    //   });
+    // }
+    //
+    // const saveBtnAim = document.getElementById('save-btn-aim');
+    // if (saveBtnAim) {
+    //   const descriptionInput = document.getElementById('description-edit-aim');
+    //   const moneyNeededInput = document.getElementById('money-needed-edit-aim');
+    //   const errorDescriptionOutput = document.getElementById('edit-aim-description-error');
+    //   const errorMoneyNeededOutput = document.getElementById('edit-aim-money-needed-error');
+    //   saveBtnAim.addEventListener('click', (e) => {
+    //     e.preventDefault();
+    //     Actions.saveEditAim({
+    //       descriptionInput,
+    //       moneyNeededInput,
+    //       errorDescriptionOutput,
+    //       errorMoneyNeededOutput,
+    //     });
+    //   });
+    // }
 
-    const editAimIcon = document.getElementById('pencil-icon-aim');
-    if (editAimIcon) {
-      editAimIcon.addEventListener('click', (e) => {
-        e.preventDefault();
-        Actions.editAim();
-        const aimInput = document.getElementById('description-edit-aim');
-        aimInput.textContent = this.#config.aim.description;
-      });
-    }
-
-    const closeEditAimIcon = document.getElementById('close-icon-aim');
-    if (closeEditAimIcon) {
-      closeEditAimIcon.addEventListener('click', (e) => {
-        e.preventDefault();
-        Actions.closeEditAim();
-      });
-    }
-
-    const saveBtnAim = document.getElementById('save-btn-aim');
-    if (saveBtnAim) {
-      const descriptionInput = document.getElementById('description-edit-aim');
-      const moneyNeededInput = document.getElementById('money-needed-edit-aim');
-      const errorDescriptionOutput = document.getElementById('edit-aim-description-error');
-      const errorMoneyNeededOutput = document.getElementById('edit-aim-money-needed-error');
-      saveBtnAim.addEventListener('click', (e) => {
-        e.preventDefault();
-        Actions.saveEditAim({
-          descriptionInput,
-          moneyNeededInput,
-          errorDescriptionOutput,
-          errorMoneyNeededOutput,
-        });
-      });
-    }
-
-    const donateBtnAim = document.getElementById('donate-btn-aim');
+    const donateBtnAim = document.getElementById('donate__btn');
     if (donateBtnAim) {
       donateBtnAim.addEventListener('click', (e) => {
         e.preventDefault();
@@ -116,7 +109,7 @@ class AuthorPage {
       });
     }
 
-    const createSubBtn = document.getElementById('create-sub-btn');
+    const createSubBtn = document.getElementById('subs__add');
     if (createSubBtn) {
       createSubBtn.addEventListener('click', (event) => {
         event.preventDefault();
@@ -124,13 +117,13 @@ class AuthorPage {
       });
     }
 
-    const updateSubBtns = document.querySelectorAll('#pencil-sub-icon');
+    const updateSubBtns = document.querySelectorAll('#sub__edit');
     for (let index = 0; index < updateSubBtns.length; index++) {
       const button = updateSubBtns[index];
       button.addEventListener('click', this.updateSubHandler);
     }
 
-    const deleteSubBtns = document.querySelectorAll('#delete-sub-icon');
+    const deleteSubBtns = document.querySelectorAll('#sub__delete');
     for (let index = 0; index < deleteSubBtns.length; index++) {
       const button = deleteSubBtns[index];
       button.addEventListener('click', this.deleteSubHandler);
@@ -139,12 +132,12 @@ class AuthorPage {
 
   deleteHandler(e) {
     e.preventDefault();
-    Actions.deletePost(e.currentTarget.parentElement.parentElement.id);
+    Actions.deletePost(e.currentTarget.parentElement.id);
   }
 
   updateHandler(e) {
     e.preventDefault();
-    const postId = e.currentTarget.parentElement.parentElement.id;
+    const postId = e.currentTarget.parentElement.id;
     router.go(URLS.editPost, {
       postId,
     }, postId);
@@ -157,14 +150,15 @@ class AuthorPage {
 
   updateSubHandler(e) {
     e.preventDefault();
-    const subscription = document.getElementById(e.currentTarget.parentElement.parentElement.id);
-    const titleContainer = subscription.querySelector('#sub-title');
+    const subscription = document.getElementById(e.currentTarget.parentElement.id);
+    console.log(subscription);
+    const titleContainer = subscription.querySelector('.sub__title--text');
     const title = titleContainer.textContent;
 
-    const descriptionContainer = subscription.querySelector('#sub-description');
+    const descriptionContainer = subscription.querySelector('.sub__description');
     const description = descriptionContainer.textContent;
 
-    const costContainer = subscription.querySelector('#sub-month_cost');
+    const costContainer = subscription.querySelector('.cost');
     const cost = costContainer.textContent;
 
     Actions.renderUpdatingSubscription(e.currentTarget.parentElement.parentElement.id, {
