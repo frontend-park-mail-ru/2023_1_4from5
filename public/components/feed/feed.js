888f4ef9e7d2fc21f0611fd9f9607f92a0e83c3c
IvanStukalov <ivanstukalov2003@gmail.com>
2023-05-02 01:35:34 +0300

    photo creator

--- public/components/feed/feed.js
+++ public/components/feed/feed.js
@@ -21,6 +21,12 @@ class Feed {
     newDiv.innerHTML = template(posts);
     this.#parent.appendChild(newDiv);
 
+    const photos = document.querySelectorAll('#feed__creator--photo');
+    for (let index = 0; index < photos.length; index++) {
+      const photo = photos[index];
+      photo.style.backgroundImage = 'url(../../images/author-photo.svg)';
+    }
+
     const likeIcons = document.querySelectorAll('.feed__like');
     for (let index = 0; index < likeIcons.length; index++) {
       const likeIcon = likeIcons[index];
