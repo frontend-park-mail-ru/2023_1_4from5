7ce59334f3f28de8b9fb149d66c53903d1681b14
IvanStukalov <ivanstukalov2003@gmail.com>
2023-05-19 22:13:24 +0300

    created mainfest for pwa

--- public/index.js
+++ public/index.js
@@ -29,9 +29,6 @@ import { router } from './modules/Router.js';
 
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('/sw.js', { scope: '/' })
-    .then((reg) => {
-      console.log('SW registered', reg.scope);
-    })
     .catch((err) => {
       console.log('SW ERR: ', err);
     });
