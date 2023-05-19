a8b24966365f1ad9b9a1a6f099eccfd9928c26f8
Alik Nigmatullin <NigAlik020503@yandex.ru>
2023-05-20 01:33:21 +0300

    continue creating of donate with youmoney

--- public/components/donateWin/donateWinStore.js
+++ public/components/donateWin/donateWinStore.js
@@ -41,26 +41,46 @@ class DonateWinStore {
     input.moneyInput.style.backgroundColor = color.field;
 
     if (!errMoneyGot) {
-      const token = await request.getHeader('/api/user/donate');
-      const donateAim = await request.post('/api/user/donate', {
-        creator_id: authorPageStore.getState().creator_info.creator_id,
-        money_count: Number(moneyCount),
-      }, token);
-      if (donateAim.ok) {
-        authorPageStore.getState().aim.money_got += Number(moneyCount);
-        authorPage.config = authorPageStore.getState();
-        donateWin.removeDonateWin();
-        authorPage.render();
-      } else {
-        input.errorOutput.innerHTML = '';
-        input.errorOutput.innerHTML = 'Некорректная сумма доната';
-        input.moneyInput.style.backgroundColor = color.error;
-      }
+      const creatorIdIn = authorPageStore.getState().creator_info.creator_id;
+      // const money = Number(moneyCount);
+      // if (responseUserId.ok) {
+      // const result = await responseUserId.json();
+      // if (result.user_id) {
+      //   userIdIn = result.user_id;
+      // }
+      input.donateWinFormLabel.value = {
+        operation: 'donate',
+        creator_id: creatorIdIn,
+        // user_id: userIdIn,
+        // money: money
+      };
+      console.log(input.donateWinForm);
+      // input.donateWinForm.submit();
+
+      // eslint-disable-next-line max-len
+      // label = {"operation" : "donate",   "creator_id" : "10b0d1b8-0e67-4e7e-9f08-124b3e32cce4",
+      // "user_id_in": "c3d5be1f-64ba-49d1-bb1d-06516c64bcba", "money":"1000"}
+
+      // const token = await request.getHeader('/api/user/donate');
+      // const donateAim = await request.post('/api/user/donate', {
+      //   creator_id: authorPageStore.getState().creator_info.creator_id,
+      //   money_count: Number(moneyCount),
+      // }, token);
+
+      authorPageStore.getState().aim.money_got += Number(moneyCount);
+      authorPage.config = authorPageStore.getState();
+      donateWin.removeDonateWin();
+      authorPage.render();
     } else {
       input.errorOutput.innerHTML = '';
-      input.errorOutput.innerHTML = errMoneyGot;
+      input.errorOutput.innerHTML = 'Некорректная сумма доната';
       input.moneyInput.style.backgroundColor = color.error;
     }
+    // } else {
+    //   input.errorOutput.innerHTML = '';
+    //   input.errorOutput.innerHTML = errMoneyGot;
+    //   input.moneyInput.style.backgroundColor = color.error;
+    // }
   }
 }
 
