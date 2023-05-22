d39ea43cdf1dd18eefd507ad6b76e0ee50725a0d
Alik Nigmatullin <NigAlik020503@yandex.ru>
2023-05-22 13:26:04 +0300

    finished adding btnShow in statistics

--- public/components/statistics/statistics.js
+++ public/components/statistics/statistics.js
@@ -27,6 +27,18 @@ class Statistics {
     newDiv.innerHTML = template(this.#config);
     this.#parent.appendChild(newDiv);
 
+    const startMonth = document.getElementById('select_date__start_month');
+    const startYear = document.getElementById('select_date__start_year');
+    const endMonth = document.getElementById('select_date__end_month');
+    const endYear = document.getElementById('select_date__end_year');
+    console.log(endYear);
+
+    startMonth.value = this.#config.selectedStartMonth;
+    startYear.value = this.#config.selectedStartYear;
+    endMonth.value = this.#config.selectedEndMonth;
+    endYear.value = this.#config.selectedStartYear;
+    console.log(endYear);
+
     const phoneInput = document.getElementById('get_money__phone');
     const sumInput = document.getElementById('get_money__sum');
     const getMoneyBtn = document.getElementById('get_money__button');
@@ -41,15 +53,12 @@ class Statistics {
       });
     });
 
-    const startMonth = document.getElementById('select_date__start_month');
-    const startYear = document.getElementById('select_date__start_year');
-    const endMonth = document.getElementById('select_date__end_month');
-    const endYear = document.getElementById('select_date__end_year');
     const selectDateErr = document.getElementById('statistics__err');
     const showButton = document.getElementById('select_date__show_button');
 
     showButton.addEventListener('click', (event) => {
       event.preventDefault();
+      console.log('click', this.#config);
       Actions.showStatistics({
         startMonth,
         startYear,
@@ -58,28 +67,6 @@ class Statistics {
         selectDateErr,
       });
     });
-
-    // photo.style.backgroundImage = 'url(../../images/author-photo.svg)';
-    //
-    // const settingsBtn = document.getElementById('settings__btn');
-    // settingsBtn.addEventListener('click', (event) => {
-    //   event.preventDefault();
-    //   this.changeNameLogin();
-    //   this.changePwd();
-    // });
-    //
-    // const fileInput = document.querySelector('#photo-upload');
-    // fileInput.addEventListener('change', (event) => {
-    //   event.preventDefault();
-    //   const files = event.target.files;
-    //   Actions.changePhoto(files[0]);
-    // });
-    //
-    // const deletePhoto = document.getElementById('settings__delete--btn');
-    // deletePhoto.addEventListener('click', (event) => {
-    //   event.preventDefault();
-    //   Actions.deletePhoto(event.target.parentElement.parentElement.id);
-    // });
   }
 }
 
